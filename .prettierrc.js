616fb950d739a5c048918fd665d6519fc2c2e223
cokkto <cocteau@live.fi>
2021-05-15 08:01:06 +0300

    start page

--- .prettierrc.js
+++ .prettierrc.js
@@ -3,14 +3,5 @@ module.exports = {
     tabWidth: 4,
     semi: true,
     singleQuote: true,
-    arrowParens: 'always',
-
-    overrides: [
-        {
-            files: '*.njk',
-            options: {
-                parser: 'html',
-            },
-        },
-    ],
+    arrowParens: 'always'
 };
